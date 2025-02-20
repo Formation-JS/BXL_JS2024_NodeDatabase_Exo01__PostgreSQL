3cb993f9676c1a51664109cba0b26e249338622c
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-20 16:04:28 +0100

    Method GetById 🐘

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Exercice 01 - Utilisation de PostgreSQL en Node");
+import chalk from 'chalk';
+import movieModel from './models/movie.model.js';
+
+console.log('Exercice 01 - Utilisation de PostgreSQL en Node');
+console.log();
+
+
+console.log(chalk.blue(' • Affiche le film "11" : '));
+
+const movie = await movieModel.getById(11);
+// console.log(movie);
+console.log(movie.name + ' - ' + movie.release.toLocaleDateString());
+console.log();
--- models/movie.model.js
+++ models/movie.model.js
@@ -1,10 +1,44 @@
+import pg from 'pg';
 
 const movieModel = {
 
     // Obtenir un film à l'aide du son identifiant.
-    getById : () => {
-        throw Error('Not implemented !');
+    getById : async (movieId) => {
+        const pool = new pg.Pool({
+            host: process.env.PGHOST,
+            port: process.env.PGPORT,
+            user: process.env.PGUSER,
+            password: process.env.PGPASSWORD,
+            database: process.env.PGDATABASE,
+            connectionTimeoutMillis: 15_000,
+            max: 25
+        });
+
+        const result = await pool.query({
+            text: `
+                SELECT M.*, G."Name" AS "Genre"
+                FROM "Movie" M
+                 JOIN "Genre" G ON M."GenreId" = G."Id"
+                WHERE M."Id" = $1
+            `,
+            values: [movieId]
+        });
+
+        if(result.rowCount < 1) {
+            return null;
+        }
+
+        return {
+            id: result.rows[0].Id,
+            name: result.rows[0].Name,
+            desc: result.rows[0].Description,
+            release: new Date(result.rows[0].ReleaseDate),
+            duration: result.rows[0].Duration,
+            rating: result.rows[0].Rating,
+            genre: result.rows[0].Genre,
+        }
     },
+
 
     // Obtenir la liste des films.
     getAll : () => {
