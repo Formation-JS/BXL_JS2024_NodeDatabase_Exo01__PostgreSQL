6fe91f59170fc7fe7de9b3a3ea0647213eb2d74b
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-20 15:26:03 +0100

    Create file structure 🦉

---
+++ models/movie.model.js
@@ -0,0 +1,30 @@
+
+const movieModel = {
+
+    // Obtenir un film à l'aide du son identifiant.
+    getById : () => {
+        throw Error('Not implemented !');
+    },
+
+    // Obtenir la liste des films.
+    getAll : () => {
+        throw Error('Not implemented !');
+    },
+
+    // Ajouter un nouveau film.
+    create : () => {
+        throw Error('Not implemented !');
+    },
+
+    // Modifier un film.
+    update : () => {
+        throw Error('Not implemented !');
+    },
+
+    // Supprimer un film.
+    delete : () => {
+        throw Error('Not implemented !');
+    }
+};
+
+export default movieModel;
